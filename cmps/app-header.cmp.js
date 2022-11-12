985354a39f2dc950cecc52a2946fecae6a172582
Refael Oren <refael.oren.dev@gmail.com>
2022-11-12 20:11:44 +0200

    changes

--- cmps/app-header.cmp.js
+++ cmps/app-header.cmp.js
@@ -1,4 +1,3 @@
-
 export default {
     template: `
         <header class="app-header">
@@ -9,11 +8,11 @@ export default {
                 <img src="assets/img/logo.png" alt="" />
                 </div>
             <nav class="links" :class="isShown">
-                <router-link to="/"><span @click="isNavOpen=!isNavOpen">Home</span></router-link>
-                <router-link to="/about"><span @click="isNavOpen=!isNavOpen">About</span></router-link>
-                <router-link to="/email/inbox"><span @click="isNavOpen=!isNavOpen">Mail</span></router-link>
-                <router-link to="/note"><span @click="isNavOpen=!isNavOpen">Keep</span></router-link>
-                <router-link to="/book"><span @click="isNavOpen=!isNavOpen">Book</span></router-link>
+                <router-link to="/"><span @click="isNavOpen=false">Home</span></router-link>
+                <router-link to="/about"><span @click="isNavOpen=false">About</span></router-link>
+                <router-link to="/email/inbox"><span @click="isNavOpen=false">Mail</span></router-link>
+                <router-link to="/note"><span @click="isNavOpen=false">Keep</span></router-link>
+                <router-link to="/book"><span @click="isNavOpen=false">Book</span></router-link>
             </nav>
             <button @click="isNavOpen=!isNavOpen" class="nav-btn-header"><i class="fa-solid fa-bars"></i></button>
         </header>
--- views/app-home.cmp.js
+++ views/app-home.cmp.js
@@ -1,7 +1,8 @@
 export default {
     template: `
         <section class="home-page">
-            <h1>Enjoy the best the world can offer...</h1>
+            <h1 class="enjoy">Enjoy the best the world can offer...</h1>
+            <h1 class="appsus">AppSuS Services</h1>
             <router-link to="/about">
                 <button>Explore</button>
             </router-link>
